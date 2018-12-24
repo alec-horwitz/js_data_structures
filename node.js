2528c0e6d338411ec673de014177e86245b47567
alec-horwitz <horwitz_alec@wheatoncollege.edu>
2018-12-24 15:11:16 -0500

    Came up with a decision for how to handle destroy. Simply don't allow class destroy and always errase the variable the instance is stored in after called destroy on it.

--- node.js
+++ node.js
@@ -1,5 +1,6 @@
 const Node = (function Node() {
-  const hardDisk = (new Array(250)).fill(null)
+  const diskSize = 250
+  const hardDisk = (new Array(diskSize)).fill(null)
   return class Node {
     constructor(data, children = [], parent = null) {
       const address = Node.findSpace()
@@ -22,11 +23,20 @@ const Node = (function Node() {
       return -1
     }
 
-    static findTheIndex (arry, obj) {
-      for (let i = 0; i < arry.length; i++) {
-        if (arry[i] === obj) {
-          return i
+    static hardDrive () {
+      return hardDisk
+    }
+
+    static findTheIndex (arry, input) {
+      if (typeof input === "object") {
+        for (let i = 0; i < arry.length; i++) {
+          if (arry[i] === input) {
+            return i
+          }
         }
+      }
+      if (!(Node.validAddresses([input]) === -1)) {
+        return input
       }
       return -1
     }
@@ -36,10 +46,13 @@ const Node = (function Node() {
         if (typeof el === "string") {
           return -1
         }
+        if ((el < 0) || !(el < diskSize)) {
+          return -1
+        }
       }
-      return valid
+      return arry
     }
-    
+
     destroy () {
       const address = Node.findTheIndex(hardDisk, this)
       if (address === -1) {
@@ -47,29 +60,32 @@ const Node = (function Node() {
       } else {
         hardDisk[address] = null
       }
+      return this
     }
 
     update (changes) {
-      if (changes.data) {
-        this.data = changes.data
-      }
-      if (changes.children) {
-        const newChild = Node.validAddresses(changes.children)
-        if (newChild === -1) {
-          console.error("ERROR: Invalid Children Values!!!");
-        } else {
-          this.children = changes.children
+      if (!(this === null)) {
+        if (changes.data) {
+          this.data = changes.data
         }
-      }
-      if (changes.parent) {
-        const newParent = Node.validAddresses([changes.parent])
-        if (newParent === -1) {
-          console.error("ERROR: Invalid Parent Value!!!");
-        } else {
-          if (newParent.length === 1) {
-            this.parent = newParent[0]
+        if (changes.children) {
+          const newChild = Node.validAddresses(changes.children)
+          if (newChild === -1) {
+            console.error("ERROR: Invalid Children Values!!!");
           } else {
+            this.children = changes.children
+          }
+        }
+        if (changes.parent) {
+          const newParent = Node.validAddresses([changes.parent])
+          if (newParent === -1) {
             console.error("ERROR: Invalid Parent Value!!!");
+          } else {
+            if (newParent.length === 1) {
+              this.parent = newParent[0]
+            } else {
+              console.error("ERROR: Invalid Parent Value!!!");
+            }
           }
         }
       }
@@ -77,17 +93,21 @@ const Node = (function Node() {
   }
 })()
 
-let newNode
-for (let i = 0; i < 251; i++) {
-  newNode = new Node("")
-  if((i % 3) === 0) {
-    newNode.update({
-      data : "steve",
-      children : ["2","trdhhfytuf",523,777,23],
-      parent : 6
-    })
-  }
-  if((i % 5) === 0) {
-    newNode.destroy()
-  }
-}
+// Example Code:
+// 
+// let newNode
+// for (let i = 0; i < 50; i++) {
+//   newNode = new Node()
+//   if((i % 5) === 0) {
+//     newNode.update({
+//       data : "steve",
+//       children : [2,249,0,23],
+//       parent : 6
+//     })
+//   }
+//
+//   if((i % 3) === 0) {
+//     newNode.destroy()
+//     newNode = null
+//   }
+// }
